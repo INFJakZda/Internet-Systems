ffd3eca16babbb44d6474dcc6a8cb0d362979ebc
INFJakZda <zdanowski78@gmail.com>
2019-03-05 23:38:19 +0100

    A1 tspi server with index

--- TPSI/src/TPSIServer.java
+++ TPSI/src/TPSIServer.java
@@ -3,25 +3,25 @@ import com.sun.net.httpserver.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TPSIServer {
-	public static void main(String[] args) throws Exception {
-		int port = 8000;
-		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-		server.createContext("/", new RootHandler());
-		System.out.println("Starting server on port: " + port);
-		server.start();
-	}
+    public static void main(String[] args) throws Exception {
+        int port = 8000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new RootHandler());
+        System.out.println("Starting server on port: " + port);
+        server.start();
+    }
 
-	static class RootHandler implements HttpHandler {
-		public void handle(HttpExchange exchange) throws IOException {
-			String response = "Hello World!";
-			exchange.getResponseHeaders().set("Content-Type", "text/plain");
-			exchange.sendResponseHeaders(200, response.length());
-			OutputStream os = exchange.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-	}
-
+    static class RootHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream os = exchange.getResponseBody();
+            os.write(Files.readAllBytes(Paths.get("src/index.html")));
+            os.close();
+        }
+    }
 }
