c06524e3831f2fe9cccb38479e10e09d497942cc
INFJakZda <zdanowski78@gmail.com>
2019-03-12 23:48:29 +0100

    Redirect

--- TPSI/src/main/java/TPSIServer.java
+++ TPSI/src/main/java/TPSIServer.java
@@ -13,6 +13,7 @@ public class TPSIServer {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/", new RootHandler());
         server.createContext("/echo", new EchoHandler());
+        server.createContext("/redirect", new RedirectHandler());
         System.out.println("Starting server on port: " + port);
         server.start();
     }
@@ -39,6 +40,18 @@ public class TPSIServer {
             OutputStream os = exchange.getResponseBody();
             os.write(json.getBytes());
             os.close();
+        }
+    }
+
+    static class RedirectHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String url = exchange.getRequestURI().getPath();
+            int status = Integer.parseInt(url.replaceAll("[^-?0-9]+", ""));
+            System.out.println(status);
+
+            exchange.getResponseHeaders().set("Location", "http://wp.pl");
+            exchange.sendResponseHeaders(status, 0);
+            exchange.close();
         }
     }
 }
